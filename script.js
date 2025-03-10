a5e903d5ec9edf6aa410f71beb200a2277515f8c
aswani-28 <aswanikunda3@gmail.com>
2025-03-10 06:45:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+
 document.querySelector('#tlines').addEventListener('click', function () {
     var elements = document.querySelector('#diss');
-    if (elements.style.marginLeft === '20rem') {
+    if (elements.style.marginLeft === '14rem') {
         elements.style.marginLeft = '-300px'
 
 
     }
     else {
-        elements.style.marginLeft = '20rem';
+        elements.style.marginLeft = '14rem';
 
     }
 });
