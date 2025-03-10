7b7863d58a35e1a2cd10caac32a84c0af4d12806
aswani-28 <aswanikunda3@gmail.com>
2025-03-10 05:51:04 +0000

    Delete imagespw/emails_files directory

--- imagespw/emails_files/script.js
+++
@@ -1,8 +0,0 @@
-// Here  Header Section Start
-let menuBar = document.getElementsByClassName("menuBar");
-let magnifyingGlass = document.getElementsByClassName("magnifyingGlass");
-
-// Function of Menu Bar Click
-function menuBarClickable() {
-
-}
